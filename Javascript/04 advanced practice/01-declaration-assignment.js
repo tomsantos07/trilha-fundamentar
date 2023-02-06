38bb28784b870f3b69a93e168a5630cc23f57a7d
tomsantos07 <xtomsantosx@live.com>
2023-02-06 15:16:58 -0400

    realiza modificações baseada no code review

---
+++ Javascript/04 advanced practice/01-declaration-assignment.js
@@ -0,0 +1,8 @@
+// Declaration
+var name
+
+// Assignment
+name = "John"
+
+// how to know what data type is it
+console.log(typeof name);
---
+++ Javascript/04 advanced practice/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+let student = {} // seu type é um object
+console.log(typeof student);
+
+//   4.1 Atribua à ela as mesmas propriedades e valores do exercício 3
+student = {
+  name: 'Gearless Joe',
+  age: 24,
+  weight: 67.5,
+  isSubscribed: false
+}
+
+//   2.2 Mostre no console a seguinte mensagem:
+//     - <name> de idade <age> pesa <weight> kg.
+//     - Atenção, substitua <name> e <weight> pelos valores de cada propriedade do objeto.
+
+console.log(`${student.name} de idade ${student.age}, pesa ${character.weight}kg.`);
