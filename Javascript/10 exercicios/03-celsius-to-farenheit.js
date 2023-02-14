6ae6d1ea46d7fd563f4cf77cf78fc1a3f8012c12
tomsantos07 <xtomsantosx@live.com>
2023-02-13 20:52:44 -0400

    refazendo o desafio com a resposta

--- Javascript/10 exercicios/03-celsius-to-farenheit.js
+++ Javascript/10 exercicios/03-celsius-to-farenheit.js
@@ -13,15 +13,15 @@ e faça a transformação de uma unidade para outra
 
 // let celsius = '26';
 // let fahrenheit = '180';
-
+/*
 function convertTemp(celsius, farenheit) {
-  Number(celsius, farenheit)
+  Number(celsius, fahrenheit)
 
   if(celsius) {
     let F = celsius * 9/5 + 32;
-    console.log(`A temperatura é ${F} Farenheit`);
+    console.log(`A temperatura é ${F} Fahrenheit`);
   } else {
-    let C = (farenheit - 32) * 5/9;
+    let C = (fahrenheit - 32) * 5/9;
     console.log(`A temperatura é ${C.toFixed(1)} graus Celsius`);
   }
 
@@ -29,3 +29,35 @@ function convertTemp(celsius, farenheit) {
 
 console.log(convertTemp(false, '180'));
 console.log(convertTemp('26', false));
+*/
+
+// construir uma função que faça a conversão de temperatura 
+function convertDegrees(degree) {
+  const celsiusExists = degree.toUpperCase().includes('C')
+  const fahrenheitExists = degree.toUpperCase().includes('F')
+
+  // fluxo de erro
+  if (!celsiusExists && !fahrenheitExists) {
+    throw new Error('Grau não identificado')
+  }
+
+  // fluxo ideal: fahrenheit to celsius
+  let updatedDegree = Number(degree.toUpperCase().replace("F", ""));
+  let formula = fahrenheit => (fahrenheit - 32) * 5 / 9
+  let degreeSign = 'C'
+
+  // fluxo alternativo: Celsius to Fahrenheit
+  if (celsiusExists) {
+    updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+    formula = celsius => celsius * 9/5 + 32
+    degreeSign = 'F'
+  }
+
+  return formula(updatedDegree) + degreeSign
+}
+try {
+  console.log(convertDegrees('10C'))
+  console.log(convertDegrees('50F'))
+} catch (error) {
+  console.log(error.message);
+}
