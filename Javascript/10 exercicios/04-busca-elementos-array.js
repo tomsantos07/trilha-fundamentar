e23231cf404ba2cf8939a633fa618e22159476d4
tomsantos07 <xtomsantosx@live.com>
2023-02-19 19:17:58 -0400

    corrige identacao

--- Javascript/10 exercicios/04-busca-elementos-array.js
+++ Javascript/10 exercicios/04-busca-elementos-array.js
@@ -10,38 +10,38 @@
 
 const booksByCategory = [
   {
-      category: "Riqueza",
-      books: [
-          {
-              title: "Os segredos da mente milionária",
-              author: "T. Harv Eker",
-          },
-          {
-              title: "O homem mais rico da Babilônia",
-              author: "George S. Clason",
-          },
-          {
-              title: "Pai rico, pai pobre",
-              author: "Robert T. Kiyosaki e Sharon L. Lechter",
-          },
-      ],
+    category: "Riqueza",
+    books: [
+      {
+        title: "Os segredos da mente milionária",
+        author: "T. Harv Eker",
+      },
+      {
+        title: "O homem mais rico da Babilônia",
+        author: "George S. Clason",
+      },
+      {
+        title: "Pai rico, pai pobre",
+        author: "Robert T. Kiyosaki e Sharon L. Lechter",
+      },
+    ],
   },
   {
-      category: "Inteligência Emocional",
-      books: [
-          {
-              title: "Você é Insubstituível",
-              author: "Augusto Cury",
-          },
-          {
-              title: "Ansiedade – Como enfrentar o mal do século",
-              author: "Augusto Cury",
-          },
-          {
-              title: "Os 7 hábitos das pessoas altamente eficazes",
-              author: "Stephen R. Covey",
-          },
-      ],
+    category: "Inteligência Emocional",
+    books: [
+      {
+        title: "Você é Insubstituível",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Ansiedade – Como enfrentar o mal do século",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Os 7 hábitos das pessoas altamente eficazes",
+        author: "Stephen R. Covey",
+      },
+    ],
   },
 ];
 
@@ -49,7 +49,7 @@ const booksByCategory = [
 const totalCategories = booksByCategory.length
 console.log(totalCategories);
 
-for(let category of booksByCategory) {
+for (let category of booksByCategory) {
   //  A  segunda category é a propriedade do objeto
   console.log(`Total de livros da categoria: ${category.category}`);
   console.log(category.books.length);
@@ -58,15 +58,15 @@ for(let category of booksByCategory) {
 function countAuthors() {
   let authors = [];
 
-  for(let category of booksByCategory) {
-    for(let book of category.books) {
-      if(authors.indexOf(book.author) == -1) {
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (authors.indexOf(book.author) == -1) {
         authors.push(book.author)
       }
     }
   }
 
-  return(authors.length);
+  return (authors.length);
 }
 
 countAuthors()
@@ -74,15 +74,15 @@ countAuthors()
 function booksOfAuthor(author) {
   let books = [];
 
-  for(let category of booksByCategory) {
-    for(let book of category.books) {
-      if(book.author == -1) {
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (book.author == -1) {
         books.push(book.title)
       }
     }
   }
 
-  return(`Livros do ${author}: ${books}`);
+  return (`Livros do ${author}: ${books}`);
 }
 
 booksOfAuthor("George S. Clason")
