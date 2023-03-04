b2a1ac2fc705f8084384d5af38e057c7c17e296f
tomsantos07 <xtomsantosx@live.com>
2023-03-04 18:20:30 -0400

    conceitos de navegacao por elementos pai, irmao e filhos

---
+++ Javascript/DOM/08 nav by elements/02 elemento filho/script.js
@@ -0,0 +1,10 @@
+const element = document.querySelector('body')
+// retorna uma node list
+// console.log(element.childNodes);
+
+// retorna uma html collection
+// console.log(element.children);
+
+console.log(element.firstElementChild);
+console.log(element.lastElementChild);
+
---
+++ Javascript/DOM/08 nav by elements/03 elemento irmao/script.js
@@ -0,0 +1,12 @@
+const element = document.querySelector('header')
+// retorna uma node list
+// console.log(element.childNodes);
+
+// retorna uma html collection
+// console.log(element.children);
+
+console.log(element.nextElementSibling);
+
+// const element2 = document.querySelector('body header')
+// console.log(element2.previousElementSibling);
+
