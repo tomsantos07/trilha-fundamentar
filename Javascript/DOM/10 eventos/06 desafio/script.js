8a83f56d1370c47da93e93d1963d757dc63a82ba
tomsantos07 <xtomsantosx@live.com>
2023-04-06 15:13:39 -0400

    conclusão do desafio modal

--- Javascript/DOM/10 eventos/06 desafio/script.js
+++ Javascript/DOM/10 eventos/06 desafio/script.js
@@ -7,6 +7,14 @@ const modalWrapper = document.querySelector('.modal-wrapper')
 openModalButton.onclick = function () {
   modalWrapper.classList.remove('invisible')
 }
-
-// Por que estou pegando esse erro???
+// Por que estou pegando esse erro??
 // script.js:8 Uncaught TypeError: Cannot read properties of null (reading 'classList') at openModalButton.onclick (script.js:8:16)
+// Erro resolvido: houve um erro de digitação na classe, estava escrito "moda-wrapper" em vez de "moda-wrapper"
+
+// pegando a tecla Esc para fechar a tela novamente
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    modalWrapper.classList.add('invisible')
+  }
+})
